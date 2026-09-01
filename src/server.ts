2f3164a06076e307d62b232dd2910b2c94cef5d1
ShvetsIgor <ishveps@icloud.com>
2026-09-01 10:27:16 +0300

    Check the database connection on startup

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,14 @@ const PORT = 3050;
 const pool = createPool();
 const app = createApp(pool);
 
+try {
+    await pool.query('SELECT 1'); 
+} catch (error) {
+    console.error("Failed to connect to the database: ", error)
+    process.exit(1);
+}
+
+
 app.listen(PORT, (error) => {
     if (error)
         throw error;
